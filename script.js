1ecc411f8092086c650bde92e4c95aab91e6e854
NATER <165908372+kurapicu@users.noreply.github.com>
2025-11-04 19:39:48 +0100

    some more stuff

--- script.js
+++ script.js
@@ -706,32 +706,55 @@
 
 
 // Counting Cards
-var count = 0;
+// var count = 0;
 
-function cc (card) {
-switch(card){
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 6:
-    count++;
-    break;
-  case 10:
-  case "J":
-  case "Q":
-  case "K":
-  case "A":
-    count--;
-    break;
-}
-var holdbet = 'Holdbet'
-if (count > 0) {
-  holdbet = 'Bet'
-}
+// function cc (card) {
+// switch(card){
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//   case 6:
+//     count++;
+//     break;
+//   case 10:
+//   case "J":
+//   case "Q":
+//   case "K":
+//   case "A":
+//     count--;
+//     break;
+// }
+// var holdbet = 'Holdbet'
+// if (count > 0) {
+//   holdbet = 'Bet'
+// }
 
-  return count + " " + holdbet;
-}
+//   return count + " " + holdbet;
+// }
 
-cc(2); cc("K"); cc(10); cc("K"); cc("A");
-console.log(cc(4)) 
+// cc(2); cc("K"); cc(10); cc("K"); cc("A");
+// console.log(cc(4)) 
+
+
+
+// Build JavaScript Objects
+// var myDog = {
+//   "name": "Buddy",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["all"]
+// };
+
+
+
+// Accessing Object Properties with Dot Notation
+// var myDog = {
+//   "name": "Buddy",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["all"]
+// };
+// var hatValue = myDog.name;
+// var legValue = myDog.legs;
+// console.log(hatValue, legValue);
